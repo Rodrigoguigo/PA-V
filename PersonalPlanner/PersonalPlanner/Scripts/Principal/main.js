2d45345df839941098e16441599125e01af86999
Rodrigoguigo <Rodrigoguigoro@gmail.com>
2019-05-26 02:37:04 -0300

    Adicionado projeto ao github

---
+++ PersonalPlanner/PersonalPlanner/Scripts/Principal/main.js
@@ -0,0 +1,11 @@
+﻿$('#btnProjeto').on('click', function () {
+    $('#projeto').show();
+    $('#hoje').hide();
+    $('#MenuAtual').html('Projeto Aplicado V');
+});
+
+$('#btnHoje').on('click', function () {
+    $('#hoje').show();
+    $('#projeto').hide();
+    $('#MenuAtual').html('Hoje');
+});
---
+++ PersonalPlanner/PersonalPlanner/Scripts/Principal/sidebar.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+
+        if ($(window).width() <= 768) {
+            $('.overlay').addClass('active');
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        }
+
+    });
+
+    $('.overlay').on('click', function () {
+        if ($(window).width() <= 768) {
+            // hide sidebar
+            $('#sidebar').removeClass('active');
+            // hide overlay
+            $('.overlay').removeClass('active');
+        }
+    });
+});
